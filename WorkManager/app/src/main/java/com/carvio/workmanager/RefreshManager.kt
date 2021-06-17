fa6949266fe33e3fb0612e3ce7f3924ca40bd562
Ömer Can Koç <63169919+omercankoc@users.noreply.github.com>
2021-06-17 21:09:33 +0300

    Update Sample.

---
+++ WorkManager/app/src/main/java/com/carvio/workmanager/RefreshManager.kt
@@ -0,0 +1,25 @@
+package com.carvio.workmanager
+
+import android.content.Context
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+
+class RefreshManager(val context: Context, workerParams: WorkerParameters) : Worker(context, workerParams) {
+
+    override fun doWork(): Result {
+
+        val getData = inputData
+        val received = getData.getInt("data",0)
+        refresh(received)
+        return Result.success()
+    }
+
+    private fun refresh(data : Int){
+        val sharedPreferences = context.getSharedPreferences("com.carvio.workmanager",Context.MODE_PRIVATE)
+        var savedData = sharedPreferences.getInt("savedData",0)
+        savedData += data
+        println("savedData : $savedData")
+        sharedPreferences.edit().putInt("savedData",savedData).apply()
+    }
+
+}
--- WorkManager/app/src/main/java/com/carvio/workmanager/WorkManagerActivity.kt
+++ WorkManager/app/src/main/java/com/carvio/workmanager/WorkManagerActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.lifecycle.Observer
 import androidx.work.*
 import java.util.concurrent.TimeUnit
 
@@ -18,52 +19,59 @@ class WorkManagerActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_work_manager)
 
-        // Get!
-        val unit = Data.Builder().putInt("unit",1).build()
+        val data = Data.Builder().putInt("data",1).build() // Veri olustur.
 
-        val constraints : Constraints = Constraints.Builder()
-            //.setRequiredNetworkType(NetworkType.CONNECTED) // aga bagli olmasi gerekli...
-            //.setRequiresCharging(false) // sarjinin full olmasi gerekli degil...
-            .build()
+        val constraints : Constraints = Constraints.Builder() // Kisitlari olustur.
+            .setRequiredNetworkType(NetworkType.CONNECTED) // Aga bagli olmasi gerekli.
+            .setRequiresCharging(false) // Sarjinin full olmasi gerekli degil.
+            .build() // Olustur.
 
-        /*
-        val workRequest : WorkRequest = OneTimeWorkRequestBuilder<CounterManager>()
-            .setConstraints(constraints)
-            .setInputData(unit)
-            //.setInitialDelay(5,TimeUnit.MILLISECONDS)
-            .build()
+        /* ---
+        // Bir defa calistir.
+        val workRequest : WorkRequest = OneTimeWorkRequestBuilder<RefreshManager>()
+            .setConstraints(constraints) // Kisitlari ekle.
+            .setInputData(data) // Veriyi yolla.
+            .setInitialDelay(100, TimeUnit.MILLISECONDS) // Gecikmeli baslat.
+            .addTag("oneTimeRequest") // Tanimlayici ekle.
+            .build() // Olustur.
 
-        WorkManager.getInstance(this).enqueue(workRequest) */
+        WorkManager.getInstance(this).enqueue(workRequest) // Calistir.
+        WorkManager.getInstance(this).cancelAllWork() // Hepsini durdur.
+        WorkManager.getInstance(this).getWorkInfosByTag("oneTimeRequest") // TAG'a gore calistir.
+        WorkManager.getInstance(this).cancelAllWorkByTag("oneTimeRequest") // TAG'a gore durdur.
+        --- */
 
-        /*
-        val periodicWorkManager : PeriodicWorkRequest = PeriodicWorkRequestBuilder<CounterManager>(15,TimeUnit.MINUTES)
-            .setConstraints(constraints)
-            .setInputData(unit)
-            .build()
+        /* ---
+        // Belli periyotlar ile calistir.
+        val periodicWorkManager : PeriodicWorkRequest = PeriodicWorkRequestBuilder<RefreshManager>(15,TimeUnit.MINUTES)
+            .setConstraints(constraints) // Kisitlari ekle.
+            .setInputData(data) // Veriyi yolla.
+            .build() // Olustur.
 
-        WorkManager.getInstance(this).enqueue(periodicWorkManager) */
+        WorkManager.getInstance(this).enqueue(periodicWorkManager) // Calistir.
+        WorkManager.getInstance(this).getWorkInfoByIdLiveData(periodicWorkManager.id).observe(this,
+            Observer {
+                if(it.state == WorkInfo.State.RUNNING){
+                    println("RUNNING WORK MANAGER.")
+                } else if(it.state == WorkInfo.State.SUCCEEDED){
+                    println("RUNNING WORK SUCCEEDED.")
+                }
+            }) // ID'ye gore calistir ve gozlemle.
+        WorkManager.getInstance(this).cancelWorkById(periodicWorkManager.id)
+        --- */
 
-        // Chaining work Manager
-        val oneTimeWorkRequest :OneTimeWorkRequest = OneTimeWorkRequestBuilder<CounterManager>()
-            .setConstraints(constraints)
-            .setInputData(unit)
-            .build()
+        /* ---
+        // Zincirlenmis gorevler.
+        val oneTimeWorkRequest :OneTimeWorkRequest = OneTimeWorkRequestBuilder<RefreshManager>()
+            .setConstraints(constraints) // Kisitlari ekle.
+            .setInputData(data) // Veriyi yolla.
+            .build() // Olustur.
 
-        WorkManager.getInstance(this).beginWith(oneTimeWorkRequest)
-            .then(oneTimeWorkRequest)
-            .then(oneTimeWorkRequest)
+        WorkManager.getInstance(this).beginWith(oneTimeWorkRequest) // Calistir.
+            .then(oneTimeWorkRequest) // Sonra calistir...
+            .then(oneTimeWorkRequest) // Sonra calistir...
             .enqueue()
 
-        buttonStart = findViewById(R.id.buttonStart)
-        textView = findViewById(R.id.textView)
-        buttonStop = findViewById(R.id.buttonStop)
-
-        buttonStart.setOnClickListener {
-
-        }
-
-        buttonStop.setOnClickListener {
-
-        }
+        --- */
     }
 }
